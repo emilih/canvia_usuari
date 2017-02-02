02f32c03dfc7a3096a1f6bf9397122d454ba13d7
emilih <emilih@gmail.com>
2017-02-02 17:18:30 +0100

    Update canvia_propietari.sh

--- canvia_propietari.sh
+++ canvia_propietari.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-IFS=$'\n'
-while IFS='\n' read usuario
+IFS=$'\n'  # Establece el separador de datos el final de línea
+while IFS='\n' read usuario # mientras encuentre un cambio de línea
 do
 sudo chown -R $usuario /net/server-sync/home/students/$usuario
 done <usuarios.txt
